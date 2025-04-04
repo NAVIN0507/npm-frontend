72bcf718a34564879e270c3ee6b7dd759a0d72b8
NAVIN0507 <navinofficial2005@gmail.com>
2025-04-04 11:51:14 +0530

    Added Package Card

--- components/Packages.tsx
+++ components/Packages.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import PackageCard from './PackageCard'
 
 const Packages = () => {
   return (
-<section className='justify-center items-center flex' id='#packages'>
+<section className='justify-center items-center flex mx-auto' id='#packages'>
 <div className='mt-10'>
-    <h1 className='text-text text-4xl'>Packages</h1>
+    <h1 className='text-text text-4xl text-center'>Packages</h1>
+    <div className='grid grid-cols-1 sm:grid-cols-3 gap-3 mt-5'>
+        <PackageCard/>
+        <PackageCard/>
+    </div>
 </div>
 </section>
   )
