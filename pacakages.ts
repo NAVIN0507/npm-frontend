bcc27355618085c7d2818c78d12a34975629e5c5
NAVIN0507 <navinofficial2005@gmail.com>
2025-04-04 15:47:11 +0530

    Modiefied listing methode

---
+++ pacakages.ts
@@ -0,0 +1,19 @@
+export const MyPackages = [
+    {
+        "id":1,
+        name:"string-utilsmns",
+        description:'🚀 String Utilities is a lightweight package for common string operations. 🔠 Easily extract initials from names or phrases. ⚡ Designed for both JavaScript and TypeScript projects.' ,
+        commend:'npm i @navin0507/string-utilsmns',
+        githubRepo:'https://github.com/NAVIN0507/getStringUtils.git',
+        npmLink:'https://www.npmjs.com/package/@navin0507/string-utilsmns'
+    },
+     {
+        "id":2,
+        name:"string-utilsmns",
+        description:'🚀 String Utilities is a lightweight package for common string operations. 🔠 Easily extract initials from names or phrases. ⚡ Designed for both JavaScript and TypeScript projects.' ,
+        commend:'npm i @navin0507/string-utilsmns',
+        githubRepo:'https://github.com/NAVIN0507/getStringUtils.git',
+        npmLink:'https://www.npmjs.com/package/@navin0507/string-utilsmns'
+    },
+
+]
