081e2388aa4de160aebca494ccafd66d89b45cd3
NAVIN0507 <navinofficial2005@gmail.com>
2025-04-04 11:21:12 +0530

    Make Hero Component Responsive

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Button } from './ui/button'
+import Image from 'next/image'
 
 const Hero = () => {
   return (
    <section className='justify-center items-center
     flex'>
-    <div className='w-11/12 h-[300px] bg-secondary rounded-xl mt-20'>
-    <div className='flex gap-4 p-4'>
-        <div className='text-text p-14'>
+    <div className='w-fit xl:w-11/12 h-fit xl:h-[300px] bg-secondary rounded-xl mt-28 '>
+    <div className='flex gap-4 relative p-14 '>
+        <div className='text-text flex flex-col items-center justify-center mx-auto xl:items-start xl:m-0'>
             <h1 className="text-4xl font-bold mb-4">Developer Tools That Just Work ⚡</h1>
   <p className="text-lg text-gray-400 mb-6">
     Simple, fast, and useful npm packages by <span className="text-[var(--color-accent)] font-semibold">Navin</span>.
@@ -24,6 +25,9 @@ const Hero = () => {
       </Button>
     </a>
   </div>
+        </div>
+        <div className='hidden xl:block xl:ml-24'>
+            <Image src="/package.png" alt='package' width={400} height={400} className='absolute top-0 -mt-28'/>
         </div>
     </div>
     </div>
