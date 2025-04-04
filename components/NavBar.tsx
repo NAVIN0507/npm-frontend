53f08235e80e7afd05e91270e56426bb8672ba23
NAVIN0507 <navinofficial2005@gmail.com>
2025-04-04 09:22:18 +0530

    Installesd ShadCn Component

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,10 +6,10 @@ const NavBar = () => {
 <header className='w-full h-20 bg-secondary'>
     <nav className='p-6 text-text flex items-center justify-between'>
         <div className='flex gap-4'>
-            <Image src={"/npm.png"} alt='npm' width={82} height={82}/>
+            <Image src="/npm.png" alt='npm' width={82} height={82}/>
             <h1 className='-ml-3 text-2xl mb-1'>@navin0507</h1>
         </div>
-        
+        <div></div>
     </nav>
 </header>
   )
