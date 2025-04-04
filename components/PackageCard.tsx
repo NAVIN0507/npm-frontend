26bdc95bc27086ce34eb4ec4335b9035e4ba6914
NAVIN0507 <navinofficial2005@gmail.com>
2025-04-04 15:27:50 +0530

    Modified css

--- components/PackageCard.tsx
+++ components/PackageCard.tsx
@@ -1,9 +1,9 @@
 "use client"
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react'
+import React, { useState } from 'react'
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
-import { Copy, GitBranch, Terminal } from 'lucide-react';
+import { Copy, CopyCheck, GitBranch, Terminal } from 'lucide-react';
 import { Button } from './ui/button';
 interface PacCardProps{
   name:string;
@@ -13,23 +13,33 @@ interface PacCardProps{
   npmLink:string;
 }
 const PackageCard = ({name , description , commend , githubRepo , npmLink}:PacCardProps) => {
+  const [hasCopied, sethasCopied] = useState(false);
+  const handleCopy = ()=>{
+    navigator.clipboard.writeText(commend)
+    sethasCopied(true)
+    setTimeout(()=>{
+      sethasCopied(false)
+    } , 2000)
+  }
   return (
-    <Link href={npmLink}>
+  
 <div className='w-[400px]  h-[470px] bg-secondary rounded-xl cursor-pointer hover:-translate-y-3 duration-500 ease-in-out'>
 <div className='relative'>
   <Image src={"/package2.png"} alt='package' width={100} height={100} className='absolute -mt-10 ml-4 animate-pulse'/>
 </div>
 <div className=' p-8 text-text text-sm'>
-  <h1 className='text-3xl mt-10 text-green-300'>{name}</h1>
+  <div className=' mt-10 bg-primary p-3 rounded-xl '>
+    <h1 className='truncate text-xl'>{name}</h1>
+    </div>
   <div className='w-[350px] h-[150px] p-6  py-2 line-clamp-4 rounded-2xl mt-5 bg-primary '>
     <p>{description}</p>
   </div>
-  <Alert className="mt-3 flex flex-col gap-1">
+  <Alert className="mt-3 flex flex-col gap-1" onClick={handleCopy}>
     
 
-  <AlertDescription className='flex gap-3'>
-    {commend}
-    <Copy className="h-4 w-4"/>
+  <AlertDescription className='flex gap-3 cursor-pointer'>
+   <p className='truncate'>{commend}</p> 
+   {hasCopied ?  <CopyCheck className='h-4 w-4' color='#2b9424'/> : <Copy className="h-4 w-4"/> }
   </AlertDescription>
 </Alert>
 <Link className='' href={githubRepo}>
@@ -37,7 +47,7 @@ const PackageCard = ({name , description , commend , githubRepo , npmLink}:PacCa
 </Link>
 </div>
 </div>
-</Link>
+
   )
 }
 
--- components/Packages.tsx
+++ components/Packages.tsx
@@ -7,8 +7,8 @@ const Packages = () => {
 <div className='mt-10 mb-20'>
     <h1 className='text-text text-4xl text-center'>Packages</h1>
     <div className='grid grid-cols-1 sm:grid-cols-3 gap-6 mt-10'>
-        <PackageCard name='string-utilsmns' description='🚀 String Utilities is a lightweight package for common string operations. 🔠 Easily extract initials from names or phrases. ⚡ Designed for both JavaScript and TypeScript projects. ' commend='npm i @navin0507/string-uitlsmns' githubRepo='https://github.com/NAVIN0507/getStringUtils.git' npmLink='https://www.npmjs.com/package/@navin0507/string-utilsmns'/>
-         <PackageCard name='string-utilsmns' description='🚀 String Utilities is a lightweight package for common string operations. 🔠 Easily extract initials from names or phrases. ⚡ Designed for both JavaScript and TypeScript projects. ' commend='npm i @navin0507/string-uitlsmns' githubRepo='https://github.com/NAVIN0507/getStringUtils.git' npmLink='https://www.npmjs.com/package/@navin0507/string-utilsmns'/>
+        <PackageCard name='string-utilsmns v: 1.0.6' description='🚀 String Utilities is a lightweight package for common string operations. 🔠 Easily extract initials from names or phrases. ⚡ Designed for both JavaScript and TypeScript projects. ' commend='npm i @navin0507/string-uitlsmns' githubRepo='https://github.com/NAVIN0507/getStringUtils.git' npmLink='https://www.npmjs.com/package/@navin0507/string-utilsmns'/>
+         <PackageCard name='string-utilsmns' description='🚀 String Utilities is a lightweight package for common string operations. 🔠 Easily extract initials from names or phrases. ⚡ Designed for both JavaScript and TypeScript projects. ' commend='npm i @navin0507/string3-uitlsmns' githubRepo='https://github.com/NAVIN0507/getStringUtils.git' npmLink='https://www.npmjs.com/package/@navin0507/string-utilsmns'/>
     </div>
 </div>
 </section>
