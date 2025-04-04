fb9f8e454afd8534956ad7dda041e38dfa879b4b
NAVIN0507 <navinofficial2005@gmail.com>
2025-04-04 11:34:29 +0530

    Completed Hero Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Button } from './ui/button'
 import Image from 'next/image'
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { Terminal } from 'lucide-react'
 
 const Hero = () => {
   return (
    <section className='justify-center items-center
     flex'>
-    <div className='w-fit xl:w-11/12 h-fit xl:h-[300px] bg-secondary rounded-xl mt-28 '>
+    <div className='w-fit xl:w-11/12 h-fit  bg-secondary rounded-xl mt-28 '>
     <div className='flex gap-4 relative p-14 '>
         <div className='text-text flex flex-col items-center justify-center mx-auto xl:items-start xl:m-0'>
             <h1 className="text-4xl font-bold mb-4">Developer Tools That Just Work ⚡</h1>
@@ -25,9 +27,16 @@ const Hero = () => {
       </Button>
     </a>
   </div>
+<Alert className="mt-10">
+  <Terminal className="h-4 w-4" />
+  <AlertTitle>Hey Dev 👋</AlertTitle>
+  <AlertDescription>
+    Explore useful and lightweight npm packages built to boost your development workflow.
+  </AlertDescription>
+</Alert>
         </div>
-        <div className='hidden xl:block xl:ml-24'>
-            <Image src="/package.png" alt='package' width={400} height={400} className='absolute top-0 -mt-28'/>
+        <div className='hidden xl:flex items-center mx-auto my-auto justify-center'>
+            <Image src="/package.png" alt='package' width={400} height={400} className='absolute animate-pulse'/>
         </div>
     </div>
     </div>
